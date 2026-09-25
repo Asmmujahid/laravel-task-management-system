d7a799c57549486879fd5013611216897d2a1f1e
Asma mujahid <asmamujahid45@gmail.com>
2026-09-25 15:04:17 +0500

    Fix HTTPS asset loading with Vite

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -20,12 +20,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         rel="stylesheet">
 
-    {{-- Custom CSS --}}
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
-      <link rel="stylesheet"  href="{{ asset('css/auth.css') }}">
-
+{{-- Vite CSS --}}
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="{{ auth()->check() ? 'has-sidebar' : 'guest-page' }}">
@@ -61,9 +57,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     
 
-    {{-- Custom JS --}}
-    <script src="{{ asset('js/main.js') }}"></script>
-    <script src="{{ asset('js/dashboard.js') }}"></script>
+
     <script src="{{ asset('js/chart.js') }}"></script>
 
     @stack('scripts')
